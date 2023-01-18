53d4eb502f8a2430e3d132d358814bfb289cf187
Bibek Neupane <bibekneupane51@gmail.com>
2023-01-18 17:56:45 +0545

    nt

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import logo from "./logo.svg";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
